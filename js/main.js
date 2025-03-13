bd3e9447068e78818dae83bf631342426cda82a5
Arthur <arthur.vanroost@icloud.com>
2025-03-13 14:53:57 +0100

    mouseover + mouseout

--- js/main.js
+++ js/main.js
@@ -4,13 +4,28 @@ card.forEach(card => {
         card.style.boxShadow = "10px 20px 5px rgb(0,0,0)"
 
     })
-});
 
-let cardbody = document.querySelectorAll(".card-body")
-cardbody.forEach(card => {
+let img = card.querySelector(".card-img-top")
+    img.addEventListener("dblclick", () =>{
+        card.remove();
+    })
+
+})
+
+let cardBody = document.querySelectorAll(".card-body")
+cardBody.forEach(card => {
     card.addEventListener("mouseover", ()=>{
         card.style.backgroundColor = "rgba(41, 40, 40, 0.33)"
+        
+    });
+    
+});
+cardBody.forEach(card => {
+    card.addEventListener("mouseout", ()=>{
+        card.style.backgroundColor="white"
         
     })
     
 });
+
+
